089b759db4b04ce130b008caa417bd4b1ad30aa7
Eduardo Marinho <eduardo.coerse@gmail.com>
2025-04-12 10:44:08 -0300

    feat: ClassProfessor

--- src/main/java/com/escola/senai/model/Aluno.java
+++ src/main/java/com/escola/senai/model/Aluno.java
@@ -1,4 +1,53 @@
 package com.escola.senai.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 public class Aluno {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String NomeAluno;
+    private String MatriculaAluno;
+    private int IdadeAluno;
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Aluno id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Aluno name
+    public String getNomeAluno() {
+        return NomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        NomeAluno = nomeAluno;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Aluno Matricula
+    public String getMatriculaAluno() {
+        return MatriculaAluno;
+    }
+
+    public void setMatriculaAluno(String matriculaAluno) {
+        MatriculaAluno = matriculaAluno;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Aluno Idade
+    public int getIdadeAluno() {
+        return IdadeAluno;
+    }
+
+    public void setIdadeAluno(int idadeAluno) {
+        IdadeAluno = idadeAluno;
+    }
 }
--- src/main/java/com/escola/senai/model/Professor.java
+++ src/main/java/com/escola/senai/model/Professor.java
@@ -1,4 +1,42 @@
 package com.escola.senai.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 public class Professor {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String NomeProfessor;
+    private String DisciplinaProfessor;
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Professor id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Professor Nome
+    public String getNomeProfessor() {
+        return NomeProfessor;
+    }
+
+    public void setNomeProfessor(String nomeProfessor) {
+        NomeProfessor = nomeProfessor;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Disciplina Professor
+    public String getDisciplinaProfessor() {
+        return DisciplinaProfessor;
+    }
+
+    public void setDisciplinaProfessor(String disciplinaProfessor) {
+        DisciplinaProfessor = disciplinaProfessor;
+    }
 }
